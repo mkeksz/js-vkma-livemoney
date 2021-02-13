27a2f5c9e2d81ef4f8f6a124da5bfb3d36588e33
mrkeksz <mrkeksz@yandex.ru>
2021-02-13 22:36:26 +0300

    Add advert

--- src/views/Main/epics/Wallets/Wallets.js
+++ src/views/Main/epics/Wallets/Wallets.js
@@ -4,6 +4,7 @@ import {Buttons} from './Buttons/Buttons'
 import {WalletsGallery} from './WalletsGallery/WalletsGallery'
 import {WalletsPlaceholder} from './WalletsPlaceholder/WalletsPlaceholder'
 import {RootPanel} from '@/roots/RootPanel/RootPanel'
+import {FixedLayout, Link} from '@vkontakte/vkui'
 
 export const Wallets = () => {
   const wallets = useSelector(({wallets}) => wallets)
@@ -11,6 +12,13 @@ export const Wallets = () => {
 
   return (
     <RootPanel centered={true} fixed={true} header={{content: 'Кошелёк'}}>
+      <FixedLayout vertical='top'>
+        <div style={{textAlign: 'center', backgroundColor: 'rgba(0,0,0,.1)'}}>
+          <Link href='https://t.me/cryptex_off' style={{display: 'block', padding: '10px 0'}}>
+              Инвестиции в криптовалюты
+          </Link>
+        </div>
+      </FixedLayout>
       {wallets.length > 0 || initialization
         ? (
           !initialization
